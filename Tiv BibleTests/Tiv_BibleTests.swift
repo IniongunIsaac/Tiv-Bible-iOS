f299e1388ea28792aa111e04ca892efa4da33584
Isaac Iniongun <iniongunisaac@gmail.com>
2020-05-14 21:31:23 +0100

    Initial Commit

---
+++ Tiv BibleTests/Tiv_BibleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tiv_BibleTests.swift
+//  Tiv BibleTests
+//
+//  Created by Isaac Iniongun on 14/05/2020.
+//  Copyright © 2020 Iniongun Group. All rights reserved.
+//
+
+import XCTest
+@testable import Tiv_Bible
+
+class Tiv_BibleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
