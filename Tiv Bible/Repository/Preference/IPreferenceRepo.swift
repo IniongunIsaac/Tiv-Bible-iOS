485c96a4d62abf5859bce688606d8d36b3525d0e
Isaac Iniongun <iniongunisaac@gmail.com>
2020-05-23 10:59:36 +0100

    feat: Repository
    - Add Preference Repo protocol and implementation.

---
+++ Tiv Bible/Repository/Preference/IPreferenceRepo.swift
@@ -0,0 +1,27 @@
+//
+//  IPreferenceRepo.swift
+//  Tiv Bible
+//
+//  Created by Isaac Iniongun on 23/05/2020.
+//  Copyright © 2020 Iniongun Group. All rights reserved.
+//
+
+import Foundation
+
+protocol IPreferenceRepo {
+    
+    var isDBInitialized: Bool { get set }
+
+    var currentTheme: String { get set }
+
+    var currentVerseId: String { get set }
+
+    var currentChapterId: String { get set }
+
+    var currentBookId: String { get set }
+
+    var currentVerseString: String { get set }
+
+    var shouldReloadVerses: Bool { get set }
+    
+}
---
+++ Tiv Bible/Repository/Preference/PreferenceRepoImpl.swift
@@ -0,0 +1,85 @@
+//
+//  PreferenceRepoImpl.swift
+//  Tiv Bible
+//
+//  Created by Isaac Iniongun on 23/05/2020.
+//  Copyright © 2020 Iniongun Group. All rights reserved.
+//
+
+import Foundation
+
+struct PreferenceRepoImpl: IPreferenceRepo {
+    
+    var preference: IPreference
+    
+    var isDBInitialized: Bool {
+        get {
+            preference.isDBInitialized
+        }
+        
+        set {
+            preference.isDBInitialized = newValue
+        }
+    }
+    
+    var currentTheme: String {
+        get {
+            preference.currentTheme
+        }
+        
+        set {
+            preference.currentTheme = newValue
+        }
+    }
+    
+    var currentVerseId: String {
+        get {
+            preference.currentVerseId
+        }
+        
+        set {
+            preference.currentVerseId = newValue
+        }
+    }
+    
+    var currentChapterId: String {
+        get {
+            preference.currentChapterId
+        }
+        
+        set {
+            preference.currentChapterId = newValue
+        }
+    }
+    
+    var currentBookId: String {
+        get {
+            preference.currentBookId
+        }
+        
+        set {
+            preference.currentBookId = newValue
+        }
+    }
+    
+    var currentVerseString: String {
+        get {
+            preference.currentVerseString
+        }
+        
+        set {
+            preference.currentVerseString = newValue
+        }
+    }
+    
+    var shouldReloadVerses: Bool {
+        get {
+            preference.shouldReloadVerses
+        }
+        
+        set {
+            preference.shouldReloadVerses = newValue
+        }
+    }
+    
+}
